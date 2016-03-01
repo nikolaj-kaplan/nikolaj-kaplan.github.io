028aa4f883cd89057b8c67ab9aba41ffb3178c87
Nikolaj Hjelm Kaplan <nikolaj.kaplan@gmail.com>
2016-03-01 22:17:57 +0100

    fixing stats

--- app/objects.ts
+++ app/objects.ts
@@ -1,3 +1,5 @@
+declare var Enumerable: any;
+
 export interface Day {
   date: string;
   players: string[];
@@ -16,14 +18,18 @@ export class Player {
   wins: number;
   goals: number;
 
-  get goalsPrMatch(): number {
-    if (this.matches == 0) return 0;
-    return this.goals / this.matches;
+  get goalsPrMatch(): string {
+    if (this.matches == 0) return '0';
+    return (this.goals / this.matches).toFixed(2);
+  }
+
+  get winsPercentageString(): string {
+    return this.winsPercentage.toFixed(0);
   }
 
   get winsPercentage(): number {
     if (this.matches == 0) return 0;
-    return this.wins / this.matches * 100;
+    return (this.wins / this.matches * 100);
   }
 
 }
@@ -34,4 +40,78 @@ export class Match {
   team2: string[];
   goals: string[];
 
+}
+
+export class Stats {
+  matches: Match[];
+  players: Player[];
+  date: string;
+
+  getPlayers(): Player[] {
+    return this.transform(this.players);
+  }
+
+  constructor(matches: Match[], players: string[]){
+    this.matches = matches;
+    this.players = players.map(p => new Player(p));
+
+    var dates = Enumerable.From(this.matches).GroupBy(match => match.date).ToArray();
+    if(dates.length == 1) {
+      this.date = dates[0].Key();
+    }
+    this.init();
+  }
+
+  init() {
+    // calculate wins
+    var winnerNames = []
+    this.matches.forEach(m => {
+      winnerNames = winnerNames.concat(this.getWinnerNames(m));
+    });
+
+    var winnerCount = Enumerable.From(winnerNames).GroupBy(winnerNames => winnerNames).Select(group => ({ name: group.Key(), wins: group.source.length }));
+
+    this.players.forEach(p => {
+      var winsObject = winnerCount.FirstOrDefault(undefined, x => x.name === p.name);
+      if (winsObject) {
+        p.wins = winsObject.wins;
+      }
+    });
+
+    //calculate goals
+    var goalCount = Enumerable.From(this.matches).SelectMany(x => x.goals).GroupBy(name => name).Select(group => ({ name: group.Key(), goals: group.source.length }));
+    this.players.forEach(p => {
+      var goalsObject = goalCount.FirstOrDefault(undefined, x => x.name === p.name);
+      if (goalsObject) {
+        p.goals = goalsObject.goals;
+      }
+    });
+
+    //calculate matches
+    this.matches.forEach(m => {
+      var team1AndTeam2 = m.team1.concat(m.team2);
+      this.players.forEach(p => {
+        if (team1AndTeam2.indexOf(p.name) > -1) p.matches++;
+      })
+    });
+    this.players = Enumerable.From(this.players).Where(p => p.matches > 0).OrderBy(p => - ((p.winsPercentage * 100) + p.goalsPrMatch)).ToArray();
+  }
+
+  getWinnerNames(match: Match) {
+    if (!match.goals) return [];
+    var team1Score = 0;
+    var team2Score = 0;
+    match.goals.forEach(name => {
+      if (match.team1.indexOf(name) > -1) team1Score++;
+      else team2Score++;
+    });
+
+    if (team1Score == team2Score) return [];
+    if (team1Score > team2Score) return match.team1;
+    else return match.team2;
+  }
+
+  transform(value: any): any {
+    return Object.keys(value).map(key => value[key]);
+  }
 }
--- app/stat/stat.component.ts
+++ app/stat/stat.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit} from 'angular2/core';
+import { Component, OnInit, Pipe, PipeTransform} from 'angular2/core';
 import { Router } from 'angular2/router';
 import {RouteParams} from 'angular2/router';
-import { Match, Player} from '../objects';
+import { Match, Player, Stats} from '../objects';
 import {FirebaseService} from '../data/firebase.service';
 
 declare var Firebase: any;
 declare var Enumerable: any;
+
 
 @Component({
   selector: 'stat',
@@ -16,9 +17,9 @@ declare var Enumerable: any;
 
 
 export class StatComponent implements OnInit {
-  players: Player[]
-  goalsPrPlayer: any;
-  matches: Match[];
+  dayStats: Stats[];
+  totalStats: Stats;
+  dataLoaded: boolean = false;
 
   constructor(
     private _firebaseService: FirebaseService,
@@ -28,69 +29,25 @@ export class StatComponent implements OnInit {
   }
 
   ngOnInit() {
-    this._firebaseService.getAllMatches()
-      .then(matches => {
-      this.matches = matches;
-      this._firebaseService.getAllPlayers()
-        .then(players => {
-        this.players = players.map(p => {
-          return new Player(p);
-        })
-        this.calculateStats();
-        this.players = Enumerable.From(this.players).OrderBy(p => - ((p.winsPercentage * 100) + p.goalsPrMatch)).ToArray();
-      })
-
-    });
-  }
-
-  calculateStats() {
-    // calculate wins
-    var winnerNames = []
-    this.matches.forEach(m => {
-      winnerNames = winnerNames.concat(this.getWinnerNames(m));
-    });
-
-    var winnerCount = Enumerable.From(winnerNames).GroupBy(winnerNames => winnerNames).Select(group => ({name: group.Key(), wins: group.source.length}));
-    this.players.forEach(p => {
-      var winsObject = winnerCount.FirstOrDefault(undefined, x => x.name === p.name);
-      if(winsObject) {
-        p.wins = winsObject.wins;
-      }
-    });
-
-    //calculate goals
-    var goalCount =Enumerable.From(this.matches).SelectMany(x => x.goals).GroupBy(name => name).Select(group => ({name: group.Key(), goals: group.source.length}));
-    this.players.forEach(p => {
-      var goalsObject = goalCount.FirstOrDefault(undefined, x => x.name === p.name);
-      if(goalsObject) {
-        p.goals = goalsObject.goals;
-      }
-    });
-
-    //calculate matches
-    this.matches.forEach(m => {
-      var team1AndTeam2 = m.team1.concat(m.team2);
-      this.players.forEach(p => {
-        if(team1AndTeam2.indexOf(p.name) > -1) p.matches++;
+    this._firebaseService.getAllMatches().then(matches => {
+      this._firebaseService.getAllPlayers().then(players => {
+        this.totalStats = new Stats(matches,players);
+        this.dayStats = Enumerable.From(matches).GroupBy(match => match.date).Select(group => new Stats(group.source, players)).ToArray();
+        this.dataLoaded = true;
       })
     });
-  }
-
- getWinnerNames(match: Match) {
-    if (!match.goals) return [];
-    var team1Score = 0;
-    var team2Score = 0;
-    match.goals.forEach(name => {
-      if (match.team1.indexOf(name) > -1) team1Score++;
-      else team2Score++;
-    });
-
-    if (team1Score == team2Score) return [];
-    if (team1Score > team2Score) return match.team1;
-    else return match.team2;
   }
 
   back() {
     this._router.navigate(['Day']);
   }
+
+  getDayStats() {
+    return this.transform(this.dayStats);
+  }
+
+  transform(value: any): any {
+    return Object.keys(value).map(key => value[key]);
+  }
+
 }
